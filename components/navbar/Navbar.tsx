fca72763eb090fca723cf57d5d5373de020b9323
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-04-29 10:07:31 +0530

    feat: UI changes to the navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -8,11 +8,11 @@ function Navbar(props: Props) {
     const {} = props
 
     return (
-        <div className='h-[70px] mx-auto max-w-7xl border-b-2 border-b-green-900 py-3 px-4 flex justify-between items-center'>
+        <div className='h-[70px] mx-auto max-w-7xl border-b-2 border-b-green-900 py-3 px-4 flex justify-between items-center mb-5'>
             <Image src={logo} alt='logo' width={100} height={40} />
             <div className='flex justify-center items-center gap-5'>
-                <p className='font-semibold text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-green-800 rounded-md text-white active:scale-95 transition-all duration-200 ease-in-out'>Getting Started</p>
-                <p className='font-semibold text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-white border-green-800 text-green-800 hover:bg-green-800 rounded-md hover:text-white active:scale-95 transition-all duration-200 ease-in-out'>Components</p>
+                <p className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-green-800 rounded-md text-white active:scale-95 transition-all duration-200 ease-in-out'>Getting Started</p>
+                <p className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-white border-green-800 text-green-800 hover:bg-green-800 rounded-md hover:text-white active:scale-95 transition-all duration-200 ease-in-out'>Components</p>
             </div>
         </div>
     )
