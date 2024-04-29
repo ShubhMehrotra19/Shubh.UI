6f5cd00392b5d337a12ea75c1527224dc1753a0e
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-04-29 16:53:13 +0530

    feat: changes made to optimize code

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -18,15 +18,15 @@ function About(props: Props) {
           <p className="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">Why install dependencies when you can directly <span className='text-slate-600'>select-copy-paste</span> your favourite components?</p>
           <ul>
             <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-pink-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+              <svg className="w-8 h-8 text-pink-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
               <span className="font-medium text-gray-500">React and Tailwind based components</span>
             </li>
             <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+              <svg className="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
               <span className="font-medium text-gray-500">Constantly updated components</span>
             </li>
             <li className="flex items-center py-2 space-x-4 xl:py-3">
-              <svg className="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
+              <svg className="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
               <span className="font-medium text-gray-500">Completely customizable code-snippets</span>
             </li>
           </ul>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar(props: Props) {
         <div className='h-[70px] mx-auto max-w-7xl border-b-2 border-b-slate-900 py-3 px-4 flex justify-between items-center mb-5'>
             <Image className='cursor-pointer active:scale-95 transition-all duration-300 ease-in-out' src={logo} alt='logo' width={100} height={40} />
             <div className='flex justify-center items-center gap-5'>
-                <button className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-slate-800 rounded-md text-white active:scale-95 transition-all duration-200 ease-in-out'>Getting Started</button>
+                <button className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 md:visible hidden bg-slate-800 rounded-md text-white active:scale-95 transition-all duration-200 ease-in-out'>Getting Started</button>
                 <button className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-white border-slate-800 text-slate-800 hover:bg-slate-800 rounded-md hover:text-white active:scale-95 transition-all duration-200 ease-in-out'>Components</button>
             </div>
         </div>
