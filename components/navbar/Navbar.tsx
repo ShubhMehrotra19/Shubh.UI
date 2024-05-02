113381cc9be162e82d42297952de37af18921766
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-05-02 18:30:09 +0530

    feat: added params and dynamic routes to the UI

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar(props: Props) {
     const {} = props
 
     return (
-        <div className='h-[70px] mx-auto max-w-7xl border-b-2 border-b-slate-900 py-3 px-4 flex justify-between items-center mb-5'>
+        <div className='h-[70px] mx-auto max-w-7xl border-b-2 border-b-slate-200 py-3 px-4 flex justify-between items-center mb-5'>
            <Link href='/'><Image className='cursor-pointer active:scale-95 transition-all duration-300 ease-in-out scale-125' src={logo} alt='logo' width={100} height={40} /></Link>
             <div className='flex justify-center items-center gap-5'>
                 <Link href='/documentation' className='block'><button className='font-semibold w-fit text-sm cursor-pointer border-2 hover:scale-[102%] px-5 py-2 bg-slate-800 rounded-md text-white active:scale-95 transition-all duration-200 ease-in-out'>Getting Started</button></Link>
