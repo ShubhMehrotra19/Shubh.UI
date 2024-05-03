988c6420fba0b727dfd2236e34b002183fdd0e07
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-05-03 16:47:35 +0530

    feat: added Navbar UIs

--- componentLibrary/Navbars/Navbar1/Navbar1_text.tsx
+++ componentLibrary/Navbars/Navbar1/Navbar1_text.tsx
@@ -6,48 +6,57 @@ function Navbar1_text(props: Props) {
     const {} = props;
 
     const code = `
-    import React from 'react';
+import React from "react";
 
-    function Navbar1() {
-    
-        let navElems = [
-            {
-                navElement: "Products",
-                href: '#'
-            },
-            {
-                navElement: "Solutions",
-                href: '#'
-            },
-            {
-                navElement: "Services",
-                href: '#'
-            },
-            {
-                navElement: "Configure",
-                href: '#'
-            }
-        ];
-    
-        return (
-            <nav className='w-full flex justify-between items-center'>
-                <div className='flex justify-center items-center gap-3'>
-                    <h2 className='font-bold text-black cursor-pointer'>Logo</h2>
-                    <div className='flex justify-center items-center gap-3'>
-                        {navElems.map((item, index) => (
-                            <a className='text-black font-medium cursor-pointer' key={index} href={item.href}>{item.navElement}</a>
-                        ))}
-                    </div>
-                </div>
-                <div className='flex justify-center items-center gap-3'>
-                    <button className='w-fit px-4 py-1 border-2 border-black rounded-md text-black hover:bg-black hover:text-white hover:scale-[102%] active:scale-95 cursor-pointer transition-all duration-300 ease-in-out'>Log in</button>
-                    <button className='w-fit px-4 py-1 border-2 border-black rounded-md bg-black text-white hover:scale-[102%] active:scale-95 cursor-pointer transition-all duration-300 ease-in-out'>Register</button>
-                </div>
-            </nav>
-        );
-    }
-    
-    export default Navbar1;
+function Navbar1() {
+  let navElems = [
+    {
+      navElement: "Products",
+      href: "#",
+    },
+    {
+      navElement: "Solutions",
+      href: "#",
+    },
+    {
+      navElement: "Services",
+      href: "#",
+    },
+    {
+      navElement: "Configure",
+      href: "#",
+    },
+  ];
+
+  return (
+    <nav className="w-full flex justify-between items-center">
+      <div className="flex justify-center items-center gap-3">
+        <h2 className="font-bold text-black cursor-pointer">Logo</h2>
+        <div className="flex justify-center items-center gap-3">
+          {navElems.map((item, index) => (
+            <a
+              className="text-black font-medium cursor-pointer"
+              key={index}
+              href={item.href}
+            >
+              {item.navElement}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-3">
+        <button className="w-fit px-4 py-1 border-2 border-black rounded-md text-black hover:bg-black hover:text-white hover:scale-[102%] active:scale-95 cursor-pointer transition-all duration-300 ease-in-out">
+          Log in
+        </button>
+        <button className="w-fit px-4 py-1 border-2 border-black rounded-md bg-black text-white hover:scale-[102%] active:scale-95 cursor-pointer transition-all duration-300 ease-in-out">
+          Register
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar1;
     
 `;
 
